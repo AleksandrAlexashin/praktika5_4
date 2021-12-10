007d7e56c2d5380d9d071dd90460ace9a4b13312
AleksandrAlexashin <sashokmetall@mail.ru>
2021-12-11 00:28:57 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika_5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass4 = new int[20];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                mass4[i] = rnd.Next(-50, 50);
+
+                Console.Write("|" + mass4[i] + "|");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            int kol = 0;
+            for (int i = 0; i < 20; i++)
+            {
+
+                if (i % 2 == 0 & mass4[i] > 0 && mass4[i] % 2 != 0)
+                                                        
+                    Console.Write("|" + mass4[i] + "|", kol++);
+                              
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("количество  нечетных положительных элементов, стоящих на четных местах {0}", kol);
+            Console.ReadKey();
+        }
+
+       
+            
+
+
+            
+    }        
+}
